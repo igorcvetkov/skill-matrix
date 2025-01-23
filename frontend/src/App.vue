c5893c171e34fe28acf8ee13d7398c84258f72cb
Igor Cvetkov <igors.cvetkovs@gmail.com>
2025-01-23 10:18:41 +0200

    added vuetify component library to frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MainLayout></MainLayout>
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> <HelloWorld msg="Welcome to Your Vue.js App" /> -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from "./components/HelloWorld.vue";
+import MainLayout from "./components/MainLayout.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    // HelloWorld,
+    MainLayout,
+  },
+};
 </script>
 
 <style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
 
-createApp(App).mount('#app')
+// Vuetify
+import "vuetify/styles";
+import "@mdi/font/css/materialdesignicons.css"; // Ensure you are using css-loader
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+// Components
+import App from "./App.vue";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+createApp(App).use(vuetify).mount("#app");
